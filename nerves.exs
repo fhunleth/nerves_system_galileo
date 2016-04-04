0c19ec3637a279a57ccc63988523348f186eb5d7
Justin Schneck <jschneck@mac.com>
2016-04-04 16:41:29 -0400

    point build_platform to module

--- nerves.exs
+++ nerves.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :nerves_system_galileo, :nerves_env,
-  type:  :system,
-  build_platform: :nerves_system_br,
+  type: :system,
+  build_platform: Nerves.System.Platforms.BR,
   bakeware: [target: "galileo", recipe: "nerves/galileo"],
   ext: [
     defconfig: "nerves_defconfig"
