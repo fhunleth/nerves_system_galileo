d48365a11f3d2bc5d92dfb3a0e350e6e43183f15
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-20 22:23:58 -0400

    Move erlinit.config here; update refs

--- nerves.exs
+++ nerves.exs
@@ -12,5 +12,8 @@ config :nerves_system_galileo, :nerves_env,
     "https://github.com/nerves-project/nerves_system_galileo/releases/download/v#{version}/nerves_system_galileo-v#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
-    defconfig: "nerves_defconfig"
+    defconfig: "nerves_defconfig",
+    package_files: [
+      "rootfs-additions"
+    ]
   ]
